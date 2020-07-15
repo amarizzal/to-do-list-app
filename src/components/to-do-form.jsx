215e70bf0dec6ff0d3e4b909573128511627002a
amarizzal <rizal.ammar91@yahoo.com>
2020-07-15 16:05:51 +0700

    Menambahkan fungsi tangkap inputan user

--- src/components/to-do-form.jsx
+++ src/components/to-do-form.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
 
-const ToDoForm = () => {
+const ToDoForm = ({addToDo}) => {
+    const [ value, setValue ] = useState('');
+
+    const handleFormSubmit = e => {
+      e.preventDefault();
+
+      if(!value) {
+        alert('Tuliskan Kegiatan Anda');
+        return;
+      }
+        
+      addToDo(value);
+      alert(value); 
+      setValue("");
+      
+    };
     return (
         <div className="add">
-                  <form action="" className="add-form">
-                    <input type="text"></input>
+                  <form action="" className="add-form" onSubmit={handleFormSubmit}>
+                    <input 
+                      type="text" 
+                      value={value} 
+                      onChange={e => setValue(e.target.value)}
+                    />
                     <button type="submit" className="header-btn">ADD</button>
                   </form>
                 </div>
     )
 }
 
+ToDoForm.propTypes = {
+  addToDo: PropTypes.func.isRequired
+};
 export default ToDoForm;
