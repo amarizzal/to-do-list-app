ab55ccf971167e0eb3bf58593c4b5cb83afbfad1
amarizzal <rizal.ammar91@yahoo.com>
2020-07-27 15:16:15 +0700

    Fix refractor Header Component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import cx from 'classnames';
 
 import Button from '../button/Button';
 
@@ -7,12 +6,10 @@ import styles from './header.module.css';
 
 const Header = ({showFormToggle , showAdd , clearToDo}) => {
     
-    const className = cx(styles.header);
-
     return (
-        <div className={className}>
+        <div className={styles.header}>
             <Button text={showAdd ?  'Finish' : 'Add' } onClick={showFormToggle}/>
-            <h1 className="Title">To Do List</h1>
+            <h1 className={styles.Title}>To Do List</h1>
             <Button text='Clear' onClick={clearToDo} color='red' />
         </div>
     )
